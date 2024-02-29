3470790b1cd7cf4f6e5b4b80467bcf6ffd4a551d
Aridr1z <chonpi99@gmail.com>
2024-02-28 20:43:45 -0600

    ni idea que sea esto
    configuarciones del IDE creo

--- cmake-build-debug/CMakeFiles/rpg.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for rpg.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/goril/OneDrive/Desktop/Jalados de github xd/School/2P RPG
+# Install script for directory: C:/Users/goril/OneDrive/Desktop/Jalados de github xd/School/2P-RPG-ARIS
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/goril/OneDrive/Desktop/Jalados de github xd/School/2P RPG/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/goril/OneDrive/Desktop/Jalados de github xd/School/2P-RPG-ARIS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
